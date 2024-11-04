2589bcb8b79a8f7648a979fd4c9d5f188174c8ff
Rogannagari1603 <ravikumarsecond391@gmail.com>
2024-11-03 23:40:52 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,12 +5,3 @@ function toggleMenu(){
     icon.classList.toggle("open");
 }
 
-var icon = document.getElementById("icon");
-icon.onclick = function(){
-  document.body.classList.toggle("dark-theme");
-  if (document.body.classList.contains("dark-theme")){
-    icon.src = "./assets/assets/moon.png";
-  } else {
-    icon.src = "./assets/assets/sun.png";
-  }
-}
